b105c6ba4cc19029e991669fc8b8fd0c75953f06
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-07-16 15:02:05 +0530

    Added login feature

--- appointment.py
+++ appointment.py
@@ -2,3 +2,8 @@ print("Hospital Appointment Booking System")
 def register_patient(name);
     print(name, "registered successfully")
     register_patient("Ali")
+
+    def login():
+        print("Login Successful")
+
+    login()
